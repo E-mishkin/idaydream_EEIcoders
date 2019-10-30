68a6dbe80ff2a1d677af59c35f10854a736320d6
ImeldaMedina <55867616+ImeldaMedina@users.noreply.github.com>
2019-10-29 22:34:10 -0700

    Add files via upload

--- idaydream/confirmation.php
+++ idaydream/confirmation.php
@@ -1,57 +1,87 @@
+<?php
+$back_ground= $_POST['background'];
+$filling= $_POST['Who_filling:'];
+$first= $_POST['First_Name:'];
+$last= $_POST['Last_Name:'];
+$street_address= $_POST['Address:'];
+$city = $_POST['City:'];
+$state = $_POST['State:'];
+$zip = $_POST['ZIP:'];
+$phone = $_POST['Phone:'];
+$email = $_POST['Email:'];
+$t_shirt = $_POST['T-shirt:'];
+$interests = $_POST['interests[]'];
+$skills = $_POST['Skills:'];
+$experience = $_POST['Experience:'];
+$references = $_POST['character[]'];
+$volunteerAvailability = $_POST['volunteerAvailability[]'];
+$motivated= $_POST['Motivated:'];
+//$hear= $_POST['Hear_about_us:'];
+$mailLists= $_POST['Mail_List'];
+$agreement= $_POST['agree'];
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>I Day Dream Volunteer Form</title>
-
-    <style>
-        h3 {
-            padding: 2em;
-        }
-    </style>
+    <title>Title</title>
+<style>
+    h3 {
+        padding: 2em;
+    }
+</style>
 </head>
 <body>
-    <h3>Thank you for completing this application form and for your interest in volunteering with us.</h3>
-    <div class="form-group m-5">
-        <table class="table">
-            <?php
-                foreach ($_POST as $key => $value) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo "<strong>$key</strong>";
-                    echo "</td>";
-                    echo "<td>";
-                    echo "<em>$value</em>";
-                    echo "</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </div>
+<h3>Thank you for completing this application form and for your interest in volunteering with us.</h3>
+<div class="form-group m-5">
+    <table class="table">
+        <?php
+        foreach ($_POST as $key => $value) {
+            echo "<tr>";
+            echo "<td>";
+            echo "<strong>$key</strong>";
+            echo "</td>";
+            echo "<td>";
+            echo "<em>$value</em>";
+            echo "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</div>
+<?php
+//send order by email
+//Note: this will probably go to your junk email
+$email = "imedina-castaneda@mail.greenriver.edu";
 
-    <?php
-    //Send form by email
-    $first = $_POST['firstName'];
-    $last = $_POST['lastName'];
-    $email = "emishkin@mail.greenriver.edu";
-    $email_body = "Volunteer Form Confirmation --\r\n";
-    $email_body .= "Name: $first $last\r\n";
-    $email_subject = "Volunteer Form Confirmation";
-    $to = $email;
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email \r\n";
-    $success = mail($to, $email_subject, $email_body, $headers);
+$email_body = "Summary --\r\n";
+$email_body = "Name: $first $last\r\n filling: $filling\r\n 
+Street Address: $street_address\r\n City: $city\r\n State: $state\r\n 
+Zip: $zip\r\n Phone: $phone\r\n E-mail: $email\r\n T-shirt: $t_shirt\r\n 
+Interests: $interests\r\n Skills: $skills\r\n Experience: $experience\r\n 
+References: $references\r\n Volunteer Availability: $volunteerAvailability\r\n 
+Motivation: $motivated\r\n Add to mailing list: $mailLists \r\n";
+$email_subject = "New form submitted";
+$to = $email;
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$success = mail($to, $email_subject,$email_body,$headers);
+//print final confirmation
+//this way
+$msg = $success ? "Your form has been  successfully submitted."
+    :"We're sorry. There was a problem with your form";
+//or this way
+if($success){
+    $msg = "Your form has been  successfully submitted.";
+}else{
+    $msg = "We're sorry. There was a problem with your form";
+}
+echo "<p>$msg</p>";
+?>
 
-    //Print final form confirmation
-    $msg = $success ? "Volunteer Form has been successfully sent to us."
-        : "We're sorry. There was a problem with your form.";
 
-    echo "<div class=\"form-group m-5\"><p>$msg</p></div>";
-
-    ?>
 
 </body>
 </html>
