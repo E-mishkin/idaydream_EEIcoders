f93876c751ebdcbc6c36db21d3f9af383b283788
KaurEkampreet <55771498+KaurEkampreet@users.noreply.github.com>
2019-10-29 17:53:32 -0700

    Add files via upload

--- youthidaydream/confirmation.php
+++ youthidaydream/confirmation.php
@@ -1,56 +1,26 @@
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$gender =$_POST['gender'];
-$ethnicity = $_POST['ethnicity'];
-$grad = $_POST['grad'];
-$interest = $_POST['interest'];
-$career =$_POST['career'];
-$birth =$_POST['birth'];
-?>
-    <!DOCTYPE html>
-    <body>
-    <h1>Thank you for visiting!</h1>
-    <h2>summary:</h2>
-    <p>Name: <?php echo "$name"; ?>
-    <p>Email: <?php echo "$email"; ?>
-    <p>Phone: <?php echo "$phone"; ?>
-    <p>Gender: <?php echo "$gender";?>
-    <p>Ethnicity: <?php echo "$ethnicity"; ?>
-    <p>Graduating Class: <?php echo "$grad"; ?>
-    <p>College of Interest: <?php echo "$interest"; ?>
-    <p>Career Aspiration: <?php echo "$career";?>
-    <p>Birthdate: <?php echo "$birth"; ?>
-    </p>
-    <pre>
-<?php
-//var_dump($_POST);
-//echo "hello world";
-//send order by email
-//notet this will probably go to ur spam
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<h3>Thank you for completing this form and becoming part of ID.A.Y Dream!</h3>
+<div class="form-group m-5">
+    <table class="table">
 
-$email = "ekamthind697@gmail.com";
-$email_body = "Order Summary --\r\n";
-$email_body .="Name: $first $last\r\n";
-$email_subject = "New Order placed";
-$to = $email;
-$headers = "From: $email\r\n";
-$headers .= "Reply-T0: $email \r\n";
-$success = mail($to, $email_subject, $email_body, $headers);
-
-//print final confirmation
-/*if ($success){
-    $msg=
-}
-else {
-    $msg = "We are "
-} */
-$msg = $success ? "Your order was successfully placed."
-    : "We are sorry. there was problem with your order.";
-echo "<p>$msg</p>";
-
-?>
-</pre>
-    </body>
-    </html>
+        <?php
+        foreach($_POST as $key => $value){
+            echo "<tr>";
+            echo "<td>";
+            echo "<strog>$key</strog>";
+            echo "<td>";
+            echo "<td>";
+            echo "<em>$value</em>";
+            echo "<td>";
+            echo "<tr>";
+        }
+        ?>
+    </table>
+</div>
+</body>
+</html>
